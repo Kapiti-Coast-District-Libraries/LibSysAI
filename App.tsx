70898406d423ad063633846cc3b142c086dc1155
Kapiti-Coast-District-Libraries <paraparaumumake@gmail.com>
2026-01-30 15:24:26 +1300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -88,7 +88,7 @@ const App: React.FC = () => {
 
   useEffect(() => {
   const KNOWLEDGE_BASE_URL =
-    'https://raw.githubusercontent.com/Kapiti-Coast-District-Libraries/LibSysAI/main/SOP/';
+'https://raw.githubusercontent.com/Kapiti-Coast-District-Libraries/LibSysAI/main/manifest.json';
 
   const syncKnowledgeBase = async () => {
     let skipped = 0;
