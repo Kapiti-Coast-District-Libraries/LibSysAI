65e54738a6388b1d4fa9e7ee56c4b440208580d3
Kapiti-Coast-District-Libraries <paraparaumumake@gmail.com>
2026-07-14 11:56:09 +1200

    Create generateIcons.cjs

---
+++ .github/generateIcons.cjs
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const iconsPath = path.join(__dirname, "public", "icons");
+
+const output = path.join(iconsPath, "icons.json");
+
+const icons = fs.readdirSync(iconsPath)
+    .filter(file =>
+        /\.(svg|png|jpg|jpeg|webp)$/i.test(file)
+    )
+    .sort();
+
+fs.writeFileSync(
+    output,
+    JSON.stringify(icons, null, 2)
+);
+
+console.log(`Generated icons.json with ${icons.length} icons`);
